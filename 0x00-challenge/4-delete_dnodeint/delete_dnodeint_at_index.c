3bf99cee1eb2d7e666ba2def1d9ac0127f8a84d1
AndersenCastaneda <andersenpiano@gmail.com>
2020-08-22 20:40:31 -0500

    Fix output

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,7 +43,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
+		(*head)->next->prev = (*head)->prev;
 		if ((*head)->next)
 			(*head)->prev->next = (*head)->next;
 		free(*head);
